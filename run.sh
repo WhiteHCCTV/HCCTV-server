0b485f0a02721d6e1622f3848fb90e673c5a9207
201702083 <201702083@o.cnu.ac.kr>
2022-04-05 23:36:13 +0900

    :green_heart: install node modules before serve or build

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 case "$1" in
+	apm) # apm ui
+		npm install --prefix ./HCCTV-apm
+		npm run serve --prefix ./HCCTV-apm
+	;;
 	dev) # dev env up
 		if [ "$2" == "" ] ; then
+		        npm install --prefix ./HCCTV-apm
+			npm run build --prefix ./HCCTV-apm
 			docker compose up --build dev_db logger_db -d
 			./check-db-ready.sh
 			docker compose up --build echo-dev nginx 
