5df3b5f62f1fa83f99bf3bc3deb8befe961c6f6a
201702083 <201702083@o.cnu.ac.kr>
2022-04-12 19:28:21 +0900

    :technologist: "apm" tag build apm product

--- run.sh
+++ run.sh
@@ -2,11 +2,25 @@
 
 case "$1" in
 	dev) # dev env up
+<<<<<<< Updated upstream
 		if [ "$2" == "" ] ; then
 			docker compose up --build
 		elif [ "$2" == "-d" ]; then
 			docker compose up --build -d
  		else
+=======
+		if [ "$2" == "apm" ] ; then
+		  	npm install --prefix ./HCCTV-apm
+			npm run build --prefix ./HCCTV-apm
+			docker compose up --build dev_db logger_db -d
+			./check-db-ready.sh
+			docker compose up --build echo-dev nginx 
+ 		elif [ "$2" == "" ] ; then
+			docker compose up --build dev_db logger_db -d
+			./check-db-ready.sh
+			docker compose up --build echo-dev nginx 
+		else
+>>>>>>> Stashed changes
 			echo "'$2' is unknwon option"
 		fi
 	;;
