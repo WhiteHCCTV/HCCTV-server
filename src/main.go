e0707ed70707ccdf275086aefd676ef04d3cded4
201702083 <201702083@o.cnu.ac.kr>
2022-05-29 16:24:27 +0900

    :sparkles: detect weights received from cli

--- src/main.go
+++ src/main.go
@@ -25,7 +25,8 @@ func checkClient(hub *Hub){
 //   @ hub의 unregister 채널에 종료한 클라이언트를 전달하고 핸들러 고루틴 종료
 func handleConnection(conn net.Conn, hub *Hub) {
 	defer conn.Close()
-	notify := make(chan error)
+	notify := make(chan error) // detect connection state
+	weights := make(chan []byte) // receive weights
 	go func() {
 		buf := make([]byte, 1024)
 		for {
@@ -37,20 +38,28 @@ func handleConnection(conn net.Conn, hub *Hub) {
 			if n > 0 {
 				fmt.Println("unexpected data: %s", buf[:n])
 			}
+			weights <- buf[:n]
 		}
 	}()
 	client := &Client{hub: hub, conn: &conn, send: make(chan []byte, 256)}
 	client.hub.register <- client
 	// 새로운 클라이언트 연결 시 연결된 모든 클라이언트에게 테스트 브로드캐스트
 	hub.broadcast<-([]byte("테스트"))
+	
+	// connection 생애 주기 동안 반복
 	for {
-		err := <-notify
-		if io.EOF == err {
-			fmt.Println("connection dropped message", err)
-			hub.unregister <- client
-			return
+		select {
+		// 연결 해제 감지
+		case err := <-notify:
+			if io.EOF == err {
+				fmt.Println("connection dropped message", err)
+				hub.unregister <- client
+				return
+			}
+		// 로컬 모델의 가중치 수신 감지
+		case receive := <- weights:
+			fmt.Println("receive",string(receive))
 		}
-		
 	}
 }
 
