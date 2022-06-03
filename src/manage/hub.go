d961c5dcc0e722c5ccd2e894040cfec8ca1d6d99
201702083 <201702083@o.cnu.ac.kr>
2022-06-03 14:10:27 +0900

    :recycle: Rebase client,hub to manage package

--- src/manage/hub.go
+++ src/manage/hub.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main
+package manage
 
 import "log"
 
@@ -11,55 +11,55 @@ import "log"
 type Hub struct {
 
 	// Registered clients.
-	clients map[*Client]bool
+	Clients map[*Client]bool
 
 	// Inbound messages from the clients.
-	broadcast chan []byte
+	Broadcast chan []byte
 
 	// Register requests from the clients.
-	register chan *Client
+	Register chan *Client
 
 	// Unregister requests from clients.
-	unregister chan *Client
+	Unregister chan *Client
 }
 
-func newHub() *Hub {
+func NewHub() *Hub {
 	return &Hub{
 		// 모든 클라이언트에게 보낼 데이터 저장 공간
-		broadcast:  make(chan []byte),
+		Broadcast:  make(chan []byte),
 		// 새로 참여하는 클라이언트
-		register:   make(chan *Client),
+		Register:   make(chan *Client),
 		// 종료하는 클라이언트
-		unregister: make(chan *Client),
+		Unregister: make(chan *Client),
 		// 현재 허브에 참여중인 클라이언트들
-		clients:    make(map[*Client]bool),
+		Clients:    make(map[*Client]bool),
 	}
 }
 
-func (h *Hub) run() {
+func (h *Hub) Run() {
 	for {
 		select {
-		case client := <-h.register:
+		case client := <-h.Register:
 			// log.Println(client.conn)
-			h.clients[client] = true
-			log.Println(len(h.clients),"명의 클라이언트 연결 상태..")
-			for client:= range h.clients {
-				log.Println(client.conn)
+			h.Clients[client] = true
+			log.Println(len(h.Clients),"명의 클라이언트 연결 상태..")
+			for client:= range h.Clients {
+				log.Println(client.Conn)
 			}
 			// 등록하게되면 허브의 clients[client]를 true로 등록한다.
-		case client := <-h.unregister:
-			if _, ok := h.clients[client]; ok {
+		case client := <-h.Unregister:
+			if _, ok := h.Clients[client]; ok {
 				// 해당 클라이언트 퇴장
-				delete(h.clients, client)
-				close(client.weight)
-				log.Println(client.conn, "클라이언트 연결 종료")
+				delete(h.Clients, client)
+				close(client.Weight)
+				log.Println(client.Conn, "클라이언트 연결 종료")
 			}
-		case message := <-h.broadcast:
+		case message := <-h.Broadcast:
 			log.Println(message)
 			
 			// 모든 클라이언트에게 전송
-			for client := range h.clients {
-				(*client.conn).Write(message)
+			for client := range h.Clients {
+				(*client.Conn).Write(message)
 			}
 			
 		}
